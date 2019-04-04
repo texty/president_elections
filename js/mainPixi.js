bbc6fd5c08901f3f5a4b381c24ef6301d6a598a5
ptrBodnar <peterbodnar1213@gmail.com>
2019-04-04 12:08:26 +0300

    changed close result label

--- js/mainPixi.js
+++ js/mainPixi.js
@@ -126,7 +126,7 @@
 			
 			var div = L.DomUtil.create('div', 'info legend'),
 				grades = [zelenskiColor, closeResultColor, poroshenkoColor],
-				labels = ["Зеленський", "Різниця менше 10%" ,"Порошенко"];
+				labels = ["Зеленський", "Різниця +- 10%" ,"Порошенко"];
 						
 			// loop through our density intervals and generate a label with a colored square for each interval
 			for (var i = 0; i < grades.length; i++) {
