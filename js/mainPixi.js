d6e0379ad9a43e5d60ee1f7ac230abda7d479381
ptrBodnar <peterbodnar1213@gmail.com>
2019-04-04 12:59:55 +0300

    stamino toner light

--- js/mainPixi.js
+++ js/mainPixi.js
@@ -72,12 +72,12 @@
 				id: 'mapbox.light'
 			}).addTo(map); */
 
-			var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+			var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 				attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 				subdomains: 'abcd',
 				maxZoom: 20,
 				ext: 'png'
-			}).addTo(map);
+			});
 
 
 			map.scrollWheelZoom.disable()
